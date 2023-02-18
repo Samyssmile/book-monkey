32dcc2d192aa30c09a58f87e5f9bfcc917d5543b
Samuel Abramov <info@abramov-samuel.de>
2023-02-18 19:50:40 +0100

    Kapitel 6 - Abgeschlossen

--- src/app/shared/book.ts
+++ src/app/shared/book.ts
@@ -2,7 +2,7 @@ export interface Book {
   isbn: string;
   title: string;
   authors: string[];
-  published?: string;
+  published?: Date;
   subtitle?: string;
   thumbnailUrl?: string;
   description?: string;
