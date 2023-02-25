d7e73e56007998211f9b637984bb2597612d48ce
Samuel Abramov <info@abramov-samuel.de>
2023-02-25 16:10:05 +0100

    Kapitel 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { BookListItemComponent } from './book-list-item/book-list-item.component';
-
 import { BookDetailsComponent } from './book-details/book-details.component';
 
 @NgModule({
-  declarations: [AppComponent, BookListComponent],
+  declarations: [
+    AppComponent,
+    BookListComponent,
+    BookListItemComponent,
+    BookDetailsComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
